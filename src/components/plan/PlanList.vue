e98e429575f726e210f052dfa7bb082777a775a0
kjy6135@naver.com <kjy6135@naver.com>
2023-05-22 11:24:04 +0900

    feat : 계획 리스트 템플릿 추가

--- src/components/plan/PlanList.vue
+++ src/components/plan/PlanList.vue
@@ -2,42 +2,14 @@
   <div>
     <div class="position-relative">
       <!-- shape Hero -->
-      <section class="section-shaped my-0">
-        <div class="shape shape-style-1 shape-default shape-skew">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <div class="container shape-container d-flex">
-          <div class="col px-0">
-            <div class="row">
-              <div class="col-lg-6">
-                <h1 class="display-3 text-white">
-                  상상만으로 설레는 여행!
-                  <span>어디로 갈지 생각해 볼까요~?</span>
-                </h1>
 
-                <base-button class="mb-3 mb-sm-0 mt-5" type="info" icon="fa fa-code" @click="moveWrite">
-                  계획 세우기
-                </base-button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
       <!-- 1st Hero Variation -->
     </div>
-    <section class="section section-lg pt-lg-0 mt--200">
+    <section class="section section-lg pt-lg-0 mt--400">
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-lg-12">
-            <!-- <div class="row row-grid">
+            <div class="row row-grid">
               <div class="col-lg-4">
                 <card class="border-0" hover shadow body-classes="py-5">
                   <icon name="ni ni-check-bold" type="primary" rounded class="mb-4"> </icon>
@@ -86,8 +58,7 @@
                   <base-button tag="a" href="#" type="warning" class="mt-4"> Learn more </base-button>
                 </card>
               </div>
-            </div> -->
-            <plan-list></plan-list>
+            </div>
           </div>
         </div>
       </div>
@@ -96,18 +67,8 @@
 </template>
 
 <script>
-import PlanList from "../components/plan/PlanList.vue";
-
 export default {
-  name: "TourPlan",
-  components: {
-    PlanList,
-  },
-
-  methods: {
-    moveWrite() {
-      this.$router.push({ name: "PlanWrite" });
-    },
-  },
+  name: "home",
+  components: {},
 };
 </script>
