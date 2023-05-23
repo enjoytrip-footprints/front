ff884dc16adc9599d683ba3572fc93dbcc8795ab
yeonhee <ydol2@naver.com>
2023-05-23 15:32:46 +0900

    refactor: view 리팩토링

--- src/views/PersonReviewView.vue
+++ src/views/PersonReviewView.vue
@@ -1,71 +1,68 @@
 <template>
-
-    <div class="position-relative">
-      <section class="section-shaped my-0">
-        <div class="shape shape-style-1 shape-default shape-skew bg-gradient-red">
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <div class="container shape-container d-flex">
-          <div class="col px-0">
-            <div class="row">
-              <div class="col-lg-6" style="margin-bottom: 10px;">
-                <h1 class="display-3  text-white">
-                  {{review.userId}} 님의 후기
-                </h1>
-              </div>
-              <div class="row" style="border-bottom: solid;">
-              <base-button 
-                style="position: absolute; background-color: white; border-color: white; color: black;
-                  right: 18px" 
-                  v-if="userInfo" 
-                  class="my-4" 
-                  @click="shareReview">
-                  여행 후기  공유하기
-                </base-button>
-              </div>
+  <div class="position-relative">
+    <section class="section-shaped my-0">
+      <div class="shape shape-style-1 shape-default shape-skew bg-gradient-red">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div class="container shape-container d-flex">
+        <div class="col px-0">
+          <div class="row">
+            <div class="col-lg-6" style="margin-bottom: 10px;">
+              <h1 class="display-3  text-white">
+                {{review.userId}} 님의 후기
+              </h1>
             </div>
-            <div style="margin-top:100px;">
-            <router-view></router-view><br /><br /></div>
+            <div class="row" style="border-bottom: solid;">
+              <base-button 
+                v-if="userInfo" 
+                style="position: absolute; background-color: white; border-color: white; color: black; right: 18px" 
+                class="my-4" 
+                @click="shareReview"
+                >여행 후기  공유하기
+              </base-button>
+            </div>
+          </div>
+          <div style="margin-top:100px;">
+            <router-view></router-view>
+            <br/><br/>
           </div>
         </div>
-        
-      </section>
-    </div>
-
+      </div>
+    </section>
+  </div>
 </template>
   
 <script>
-  import { mapState, mapActions } from "vuex";
-  import MyReviewItem from "@/components/user/MyReviewItem";
-  
-  export default {
-    name: "PersonReviewView",
-    components: {
-        MyReviewItem,
+import { mapState, mapActions } from "vuex";
+import MyReviewItem from "@/components/user/MyReviewItem";
+
+export default {
+  name: "PersonReviewView",
+  components: {
+      MyReviewItem,
+  },
+  computed: {
+    ...mapState("userStore", ["userInfo"]),
+    ...mapState("reviewStore", ["reviews","review"]),
+  },
+  methods: {
+    ...mapActions("reviewStore", ["getPersonReviewList"]),
+    shareReview() {
+      this.$router.push({ name: "reviewShare" });
     },
-    computed: {
-      ...mapState("userStore", ["userInfo"]),
-      ...mapState("reviewStore", ["reviews","review"]),
-    },
-    methods: {
-      ...mapActions("reviewStore", ["getPersonReviewList"]),
-  
-      shareReview() {
-        this.$router.push({ name: "reviewShare" });
-      },
-    },
-    mounted() {
-      this.getPersonReviewList(this.review.userId);
-    },
-  };
-    </script>
-    
-    <style></style>
+  },
+  mounted() {
+    this.getPersonReviewList(this.review.userId);
+  },
+};
+</script>
+
+<style></style>
