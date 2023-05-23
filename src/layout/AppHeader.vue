051de8a587d4825386d70ab9d0ec73d10bba374a
yeonhee <ydol2@naver.com>
2023-05-23 15:07:56 +0900

    refactor: header footer 리팩토링

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -43,17 +43,17 @@
             </router-link>
           </div>
         </base-dropdown>
+
         <base-dropdown tag="li" class="nav-item">
           <a slot="title" href="#" class="nav-link" data-toggle="dropdown" role="button">
             <i class="ni ni-collection d-lg-none"></i>
             <span class="nav-link-inner--text">여행 후기</span>
           </a>
-          <!-- <router-link to="/landing" class="dropdown-item">Landing</router-link> -->
-          <!-- <router-link to="/profile" class="dropdown-item">Profile</router-link> -->
           <router-link to="/hotReview/list" class="dropdown-item">인기 여행 후기</router-link>
           <router-link to="/review/list" class="dropdown-item">여행 후기</router-link>
           <router-link to="/board/list" class="dropdown-item">게시판</router-link>
         </base-dropdown>
+
         <base-dropdown v-if="!userInfo" ag="li" class="nav-item">
           <a slot="title" href="#" class="nav-link" data-toggle="dropdown" role="button">
             <i class="ni ni-collection d-lg-none"></i>
@@ -62,6 +62,7 @@
           <router-link to="/login" class="dropdown-item">로그인</router-link>
           <router-link to="/register" class="dropdown-item">회원가입</router-link>
         </base-dropdown>
+
         <base-dropdown v-if="userInfo" tag="li" class="nav-item">
           <a slot="title" href="#" class="nav-link" data-toggle="dropdown" role="button">
             <i class="ni ni-collection d-lg-none"></i>
@@ -71,51 +72,11 @@
           <router-link to="/myReview/list" class="dropdown-item">내 후기</router-link>
         </base-dropdown>
       </ul>
-      <!-- <ul class="navbar-nav align-items-lg-center ml-lg-auto">
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://www.facebook.com/creativetim" target="_blank" rel="noopener"
-                       data-toggle="tooltip" title="Like us on Facebook">
-                        <i class="fa fa-facebook-square"></i>
-                        <span class="nav-link-inner--text d-lg-none">Facebook</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://www.instagram.com/creativetimofficial"
-                       target="_blank" rel="noopener" data-toggle="tooltip" title="Follow us on Instagram">
-                        <i class="fa fa-instagram"></i>
-                        <span class="nav-link-inner--text d-lg-none">Instagram</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://twitter.com/creativetim" target="_blank" rel="noopener"
-                       data-toggle="tooltip" title="Follow us on Twitter">
-                        <i class="fa fa-twitter-square"></i>
-                        <span class="nav-link-inner--text d-lg-none">Twitter</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="https://github.com/creativetimofficial/vue-argon-design-system"
-                       target="_blank" rel="noopener" data-toggle="tooltip" title="Star us on Github">
-                        <i class="fa fa-github"></i>
-                        <span class="nav-link-inner--text d-lg-none">Github</span>
-                    </a>
-                </li>
-                <li class="nav-item d-none d-lg-block ml-lg-4">
-                    <a href="https://www.creative-tim.com/product/vue-argon-design-system" target="_blank" rel="noopener"
-                       class="btn btn-neutral btn-icon">
-                <span class="btn-inner--icon">
-                  <i class="fa fa-cloud-download mr-2"></i>
-                </span>
-                        <span class="nav-link-inner--text">Download</span>
-                    </a>
-                </li>
-            </ul> -->
     </base-nav>
   </header>
 </template>
 <script>
-import { mapState, mapActions } from "vuex";
-
+import { mapState } from "vuex";
 import BaseNav from "@/components/base/BaseNav";
 import BaseDropdown from "@/components/base/BaseDropdown";
 import CloseButton from "@/components/base/CloseButton";
